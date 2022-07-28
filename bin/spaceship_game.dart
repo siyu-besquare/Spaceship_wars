5c03f95fd63a8bbd3d6b9b99572d98d31254cf64
Siyu <si.yu@besquare.com.my>
2022-07-28 16:47:10 +0800

    first commit

---
+++ bin/spaceship_game.dart
@@ -0,0 +1,95 @@
+import "dart:io";
+import "dart:math";
+
+Random random = Random();
+
+void main() {
+  SpaceShip sp1 = ArmoredSpaceShip(0.40, 100);
+  SpaceShip sp2 = HighSpeedSpaceShip(true, 50);
+  BattleField().startBattle(sp1, sp2);
+}
+
+abstract class SpaceShip {
+  double health;
+  double firePower;
+  double enemyFirePower;
+
+  void hit();
+
+  void isDestroyed();
+
+  SpaceShip(this.health, this.firePower, this.enemyFirePower);
+}
+
+class ArmoredSpaceShip implements SpaceShip {
+  @override
+  double health = 1000;
+  @override
+  double firePower = 100;
+  @override
+  double enemyFirePower;
+  double maxArmorPower;
+  ArmoredSpaceShip(this.maxArmorPower, this.enemyFirePower);
+
+  @override
+  void hit() {
+    double randomArmorPower = Random().nextDouble() * maxArmorPower;
+    double damage = enemyFirePower * (1 - randomArmorPower);
+    health = health - damage;
+    print("Spaceship 1 is hit!");
+  }
+
+  @override
+  void isDestroyed() {
+    print("Game Over. Spaceship 1 is dead...");
+  }
+}
+
+class HighSpeedSpaceShip implements SpaceShip {
+  @override
+  double health = 250;
+  @override
+  double firePower = 100;
+  @override
+  double enemyFirePower;
+  bool dodging;
+  HighSpeedSpaceShip(this.dodging, this.enemyFirePower);
+
+  @override
+  void hit() {
+    dodging = random.nextBool();
+    double damage = enemyFirePower;
+    if (dodging == false) {
+      health = health - damage;
+      print("Spaceship 2 is hit!");
+    } else if (dodging == true) {
+      health = health + 0;
+      print("Spaceship 2 dodged the attack!");
+    }
+  }
+
+  @override
+  void isDestroyed() {
+    print("GAME OVER. Spaceship 2 is dead...");
+  }
+}
+
+class BattleField {
+  void startBattle(SpaceShip sp1, SpaceShip sp2) {
+    while (sp1.health > 0 && sp2.health > 0) {
+      sp1.hit();
+      print("Spaceship 1 health: ${sp1.health}\n");
+      sleep(Duration(seconds: 1));
+      sp2.hit();
+      print("Spaceship 2 health: ${sp2.health}\n");
+      sleep(Duration(seconds: 1));
+    }
+    print("Spaceship 1 health: ${sp1.health} HP");
+    print("Spaceship 2 health: ${sp2.health} HP");
+    if (sp1.health <= 0) {
+      ArmoredSpaceShip(0.40, 100).isDestroyed();
+    } else if (sp2.health <= 0) {
+      HighSpeedSpaceShip(true, 50).isDestroyed();
+    }
+  }
+}
---
+++ test/spaceship_game_test.dart
@@ -0,0 +1,8 @@
+import 'package:spaceship_game/spaceship_game.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
